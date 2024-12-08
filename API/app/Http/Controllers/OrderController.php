b3fce9b5f4d6453048924b3e8378ed300aca865f
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-12-08 12:05:09 +0700

    asdasd

--- API/app/Http/Controllers/OrderController.php
+++ API/app/Http/Controllers/OrderController.php
@@ -16,12 +16,12 @@ class OrderController extends BaseController
         $customer_name = $request->input("customer_name");
         $order_date = $request->input("order_date");
         $items = $request->input("items");
-    
+
         $orderId = DB::table('orders')->insertGetId([
             'customer_name' => $customer_name,
             'order_date' => $order_date,
         ]);
-    
+
         foreach ($items as $item) {
             $quantity = $item['quantity'];
             $productId = $item['product_id'];
@@ -42,7 +42,7 @@ class OrderController extends BaseController
             }
 
             $price = $product->price;
-    
+
             DB::insert('insert into order_items (order_id, product_id, quantity, price) values (?, ?, ?, ?)', [$orderId, $productId, $quantity, $price * $quantity]);
 
             $product->current_stock = $product->current_stock - $quantity;
@@ -51,9 +51,45 @@ class OrderController extends BaseController
 
         return "Order created successfully";
     }
-    
 
-    public function getAll(){
-        return DB::select('select * from orders LEFT JOIN order_items ON orders.id = order_items.product_id');
+
+    public function getAll() {
+        $result = DB::select("
+           SELECT
+                CONCAT(
+                    '{\"customer_name\":\"', orders.customer_name, '\",',
+                    '\"order_date\":\"', orders.order_date, '\",',
+                    '\"total_items\":', SUM(order_items.quantity), ',',
+                    '\"total_price\":', SUM(order_items.price * order_items.quantity), ',',
+                    '\"items\":[',
+                        GROUP_CONCAT(
+                            CONCAT(
+                                '{\"product_id\":', order_items.product_id, ',',
+                                '\"product_name\":\"', products.name, '\",',
+                                '\"subtotal_price\":', order_items.price, ',',
+                                '\"quantity\":', order_items.quantity, '}'
+                            )
+                        ),
+                    ']}'
+                ) AS layered_response
+            FROM
+                orders
+            JOIN
+                order_items ON orders.id = order_items.order_id
+            JOIN
+                products ON order_items.product_id = products.id
+            GROUP BY
+                orders.id, orders.customer_name, orders.order_date;
+
+
+        ");
+
+        $jsonResult = [];
+        foreach ($result as $row) {
+            $jsonResult[] = json_decode($row->layered_response, true);
+        }
+
+        return $jsonResult;
     }
+
 }
